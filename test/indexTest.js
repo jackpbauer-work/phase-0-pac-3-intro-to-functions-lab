0feba6bee53b61cfb30eb8650c64e49b77c7db6b
Jack Bauer <jackbauer.work@gmail.com>
2023-02-14 17:01:36 -0500

    complete lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,9 @@
 require ( './root.js' );
 
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
+describe("shout(string)", function() {
+  it("receives one argument and returns it in all caps", function() {
+    expect(shout("hello")).toEqual("HELLO");
   })
 })
 
